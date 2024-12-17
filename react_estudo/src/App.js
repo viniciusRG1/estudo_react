99b50defdf0950e1515984bf1a977660b30bfd5d
vinicius <viniciusxsantosx@hotmail.com.br>
2024-12-17 10:53:14 -0300

    estudo

---
+++ react_estudo/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <div>
+      <p>
+        amo programar
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
